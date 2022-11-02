89a554c5c1bdae0d088f123540962bd2aeff4c7a
DanilkaXS <83085982+DanilkaXS@users.noreply.github.com>
2022-11-02 12:25:40 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
+    return render_template('index.html')
+
+
+@app.route('/postrequest')
+def post_req():
     return render_template('index.html')
 
 
